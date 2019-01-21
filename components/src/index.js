ff84cbb408c95e1cfcf95b806577acb3929636a2
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-21 19:58:40 +0200

    Initial comments html structure. Insert semantic ui css

--- components/src/index.js
+++ components/src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>
+  return (
+    <div className="ui container comments">
+      <h3 className="ui dividing header">Comments</h3>
+      <div className="comment">
+        <a className="avatar" href="/">
+          <img src="" alt="avatar"/>
+        </a>
+        <div className="content">
+          <a className="author" href="/">Pesho</a>
+          <div className="metadata">
+            <span className="date">Today at 5:42PM</span>
+          </div>
+          <div className="text">
+            How artistic!
+          </div>
+          {/* <div className="actions">
+            <a className="reply" href="/">Reply</a>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 )
