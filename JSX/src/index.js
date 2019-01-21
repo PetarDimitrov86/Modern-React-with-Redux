191d0751f2ee40e4f5f0c31d039272569e005b57
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-21 17:25:24 +0200

    Write inline styling in JSX

--- JSX/src/index.js
+++ JSX/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a React Component
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white'}}>
+                Submit
+            </button>
+        </div>
+    );
 }
 
 // Tke the React Component and show it on the screen
