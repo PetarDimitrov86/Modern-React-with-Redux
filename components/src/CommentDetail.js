66f5fe7a8a20eedfbc57d2976dd308ba8a225400
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-21 20:57:28 +0200

    Pass and receive Props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a className="avatar" href="/">
@@ -9,7 +9,7 @@ const CommentDetail = () => {
       </a>
       <div className="content">
         <a className="author" href="/">
-          Pesho
+          {props.author}
         </a>
         <div className="metadata">
           <span className="date">Today at 5:42PM</span>
--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import faker from 'faker'
 
 const App = () => {
   return (
     <div className="ui container comments">
       <h3 className="ui dividing header">Comments</h3>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author={`${faker.name.firstName()} ${faker.name.lastName()}`} />
+      <CommentDetail author={`${faker.name.firstName()} ${faker.name.lastName()}`} />
+      <CommentDetail author={`${faker.name.firstName()} ${faker.name.lastName()}`} />
     </div>
   );
 }
