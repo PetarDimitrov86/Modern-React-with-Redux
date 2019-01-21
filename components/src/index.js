6f05a3a4113c90879930541b0d4837c55edaf6cc
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-21 19:28:29 +0200

    Initial setup for components project

---
+++ components/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hi there!</div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
