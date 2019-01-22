c4f28438176e8ef5762935ce6dae5a6c4b88a939
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-22 11:07:27 +0200

    Pass multiple props with dynamically generated content

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a className="avatar" href="/">
-        <img src={faker.image.avatar()} alt="avatar" />
+        <img src={props.avatar} alt="avatar" />
       </a>
       <div className="content">
         <a className="author" href="/">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 5:42PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">How artistic!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail'
-import faker from 'faker'
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
+
+function getDaysAgo() {
+  let timeDiff = Math.round((new Date().getTime() - faker.date.past().getTime()) / 1000);
+  let days = Math.floor(timeDiff / 86400);
+  timeDiff -= days * 86400;
+
+  let hours = Math.floor(timeDiff / 3600) % 24;
+  timeDiff -= hours * 3600;
+
+  let minutes = Math.floor(timeDiff / 60) % 60;
+
+  return `${days} days, ${hours} hours and ${minutes} minutes ago`;
+}
+
+function getRandomFullName() {
+  return `${faker.name.firstName()} ${faker.name.lastName()}`;
+}
 
 const App = () => {
   return (
     <div className="ui container comments">
       <h3 className="ui dividing header">Comments</h3>
-      <CommentDetail author={`${faker.name.firstName()} ${faker.name.lastName()}`} />
-      <CommentDetail author={`${faker.name.firstName()} ${faker.name.lastName()}`} />
-      <CommentDetail author={`${faker.name.firstName()} ${faker.name.lastName()}`} />
+      <CommentDetail
+        author={getRandomFullName()}
+        timeAgo={getDaysAgo()}
+        content={faker.lorem.sentences()}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={getRandomFullName()}
+        timeAgo={getDaysAgo()}
+        content={faker.lorem.sentences()}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={getRandomFullName()}
+        timeAgo={getDaysAgo()}
+        content={faker.lorem.sentences()}
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
-}
+};
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
