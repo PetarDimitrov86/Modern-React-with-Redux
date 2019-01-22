99974f47d694414bd988ce04b58f24c1eb109ce6
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-22 15:06:27 +0200

    Convert functional component to class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,15 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi there!</div>;
+class App extends Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    )
+
+    return <div>Latitude: </div>
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
