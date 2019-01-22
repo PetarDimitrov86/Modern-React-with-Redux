aedcc3526323220fb2e5e4f06391c2ab07a7b492
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-22 18:41:10 +0200

    Extract options to config objects

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,15 @@
-import React from "react";
+import React from 'react';
+
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake'
+  }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 0) {
@@ -8,10 +19,17 @@ const getSeason = (lat, month) => {
   }
 };
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
   const season = getSeason(props.latitute, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
 
-  return <div>Season Display</div>;
-}
+  return (
+    <div>
+      <i className={`${iconName} icon massive`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive`} />
+    </div>
+  );
+};
 
 export default SeasonDisplay;
