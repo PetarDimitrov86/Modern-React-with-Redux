306b1231ec6ca94bbd9a9162d99cdedfc676220e
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-23 18:47:00 +0200

    Destruct the image object and use description as alt image value

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const ImageList = props => {
-  const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.thumb} alt="search result" />
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.thumb} alt={description} />
   });
 
   return <div>{images}</div>;
