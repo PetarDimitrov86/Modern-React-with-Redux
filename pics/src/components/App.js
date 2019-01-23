5ae7f881a895a2b0e1a4cdb2e264ace181616ae5
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-23 13:29:50 +0200

    Add search input field and stylying

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,9 @@ import SearchBar from './SearchBar'
 
 const App = () => {
   return (
-    <SearchBar />
+    <div className="ui container" style={{ marginTop: '10px' }}>
+<     SearchBar />
+    </div>
   );
 };
 
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <div>Search Bar</div>
-  }
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+        <div className="field">
+          <label>Image Search</label>
+          <input type='text' />
+        </div>
+        </form>
+      </div>
+    );
+  };
 }
 
 export default SearchBar;
