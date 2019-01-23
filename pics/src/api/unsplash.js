d5c8225070d13e58f9f4a9d9d4666fc37ab4ee00
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-23 18:00:16 +0200

    Untrack unsplash.js

--- pics/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID REPLACE-WITH-UNSPLASH-ACCESS-KEY'
-  }
-});
