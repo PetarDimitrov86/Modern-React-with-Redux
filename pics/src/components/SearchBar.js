5bdcc168829937cdad0ed30017e62c9e01f31315
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-23 13:47:17 +0200

    Create event handlers. Use two methods

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
         <div className="field">
           <label>Image Search</label>
-          <input type='text' />
+          <input type='text' onChange={this.onInputChange} />
+          {/* // Alternative version
+          <input type='text' onChange={(e) => console.log(e.target.value)} /> */}
         </div>
         </form>
       </div>
