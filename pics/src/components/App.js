7d84cffc2e3cf05afb2e01c32c8b62819c3c573a
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-23 16:57:53 +0200

    Use simple get-then chain function

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,24 +4,29 @@ import SearchBar from './SearchBar';
 
 class App extends Component {
   onSearchSubmit(term) {
-    axios.get(`https://api.unsplash.com/search/photos`, {
-      params: {
-        query: term
-      },
-      headers: {
-        Authorization: 'Client-ID 1fd7b73165210f90c272fbc7a23c22479da7254e2ae144dc29c39f1dca294565'
-      }
-    });
+    axios
+      .get(`https://api.unsplash.com/search/photos`, {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization:
+            'Client-ID 1fd7b73165210f90c272fbc7a23c22479da7254e2ae144dc29c39f1dca294565'
+        }
+      })
+      .then(response => {
+        console.log(response.data.results.length);
+      });
   }
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-      {/* The onSubmit is a name that we chose. It can be any name we want */}
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        {/* The onSubmit is a name that we chose. It can be any name we want */}
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
-  };
+  }
 }
 
 export default App;
