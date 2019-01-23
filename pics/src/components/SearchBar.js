1efcc232e30ca1d5f49ad3f158f5d7d33ff66eec
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-23 14:40:24 +0200

    Convert uncontrolled element to controlled element

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
-        <div className="field">
-          <label>Image Search</label>
-          <input type='text' onChange={this.onInputChange} />
-          {/* // Alternative version
+          <div className="field">
+            <label>Image Search</label>
+            <input type='text' 
+            value={this.state.term} 
+            onChange={(e) => this.setState({ term: e.target.value })} 
+            />
+            {/* // Alternative version
           <input type='text' onChange={(e) => console.log(e.target.value)} /> */}
-        </div>
+          </div>
         </form>
       </div>
     );
