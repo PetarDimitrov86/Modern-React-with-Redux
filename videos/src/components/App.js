7a231d058c9f4e29d5af408738aec121988257a7
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-24 14:00:01 +0200

    Initial structure for exercise video application

---
+++ videos/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div><SearchBar /></div> 
+    )
+  }
+}
+
+export default App;
