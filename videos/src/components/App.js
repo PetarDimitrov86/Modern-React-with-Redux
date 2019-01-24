fd4f24bcdd6b98fdd2a5a749ac69b2123ae5e45e
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-24 20:02:42 +0200

    Minor tweaks and adjustments to improve user experience

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -14,6 +14,10 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.onQuerySubmit('news');
+  }
+
   onQuerySubmit = async query => {
     const response = await youtube.get('/search', {
       params: {
@@ -21,7 +25,10 @@ class App extends Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items, 
+      selectedVideo: response.data.items[0] 
+    });
   };
 
   onVideoSelect = video => {
@@ -32,11 +39,19 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onQuerySubmit} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
