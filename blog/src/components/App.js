0dcc474e5c942342aeb18868b7190c924c3913ec
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-28 15:06:08 +0200

    Add PostList component basic setup

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
   return (
-<div className="ui container">App</div>
-  ) 
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
---
+++ blog/src/components/PostList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+  render() {
+    return <div>Post List</div>;
+  }
+}
+
+export default PostList; 
