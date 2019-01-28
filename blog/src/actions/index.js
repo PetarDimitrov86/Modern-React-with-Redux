002ba60d47119132adb9d4f4a33a872f641f2403
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-28 16:31:01 +0200

    Include jsonPlaceholder api. Implement Redux Thunk for Actions Creators

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,7 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS'
-  };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
