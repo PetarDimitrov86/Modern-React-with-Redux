461553e37305da073d63022b5567dbc68343976c
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-29 17:53:13 +0200

    Create header that is always displayed with Link component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from '../components/streams/StreamCreate';
 import StreamEdit from '../components/streams/StreamEdit';
 import StreamDelete from '../components/streams/StreamDelete';
 import StreamList from '../components/streams/StreamList';
 import StreamShow from '../components/streams/StreamShow';
+import Header from '../components/Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
