04a5f8572c4e2394f62161bd347a235814a293cb
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-29 17:26:55 +0200

    Wire up routes using Router component.

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={Page1} />
-          <Route path="/pagetwo" component={Page2} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
