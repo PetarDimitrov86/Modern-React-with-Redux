5ed72b77696f0f34b9aeee9498767bf81bb343dc
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-29 13:20:23 +0200

    Get unique user ids. Reduce number of get requests

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,12 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -7,12 +14,20 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+// Memoized approach. Will not refresh data if it was updated.
+// import _ from 'lodash';
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -24,4 +19,4 @@ const mapStateToPros = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 }
 
-export default connect(mapStateToPros, { fetchUser })(UserHeader);
+export default connect(mapStateToPros)(UserHeader);
