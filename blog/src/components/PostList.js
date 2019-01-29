6143294bf5dc40ac33f126e9e233a241cb9aea0b
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-01-29 10:30:49 +0200

    Add UserHeader component. Create fetch user action

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
   componentDidMount() {
@@ -17,6 +18,7 @@ class PostList extends Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
+  render() {
+    return (
+      <div>User Header</div>
+    );
+  };
+}
+
+export default connect(null, { fetchUser })(UserHeader);
