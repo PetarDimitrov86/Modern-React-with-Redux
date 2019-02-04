ae040a8b4267aaa083aa549cc7e30f789e45deab
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-04 11:58:31 +0200

    Switch to Router and use history. Basic programmatic navigation

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
   DELETE_STREAM
 } from './types';
 import streams from '../apis/streams';
+import history from '../history';
 
 export const signIn = userId => {
   return {
@@ -28,6 +29,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId } );
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from '../components/streams/StreamCreate';
 import StreamEdit from '../components/streams/StreamEdit';
 import StreamDelete from '../components/streams/StreamDelete';
 import StreamList from '../components/streams/StreamList';
 import StreamShow from '../components/streams/StreamShow';
 import Header from '../components/Header';
+import history from '../history';
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -19,7 +20,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
