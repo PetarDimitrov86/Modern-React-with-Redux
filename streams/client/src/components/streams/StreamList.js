8e9198761ce5b7472db21ce49e46ad16a3a06943
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-04 11:11:28 +0200

    Pass logged in user id to Stream list. Render edit/delete buttons

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -7,10 +7,26 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin = stream => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>
+            Edit
+          </button>
+          <button className='ui button negative'>
+            Delete
+          </button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
             {stream.title}
@@ -34,7 +50,10 @@ class StreamList extends Component {
 };
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
-}
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
+};
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
