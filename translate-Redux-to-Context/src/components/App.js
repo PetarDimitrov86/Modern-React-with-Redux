c1ce0de0df2e629b52967ade58d62f4983afc01a
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-05 17:27:51 +0200

    Move logic from App to LanguageContext

--- translate-Redux-to-Context/src/components/App.js
+++ translate-Redux-to-Context/src/components/App.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
-import LanguageSelector from './LanguageSelector';
-import LanguageContext from '../context/LanguageContext';
+import { LanguageStore } from '../context/LanguageContext';
 import ColorContext from '../context/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends Component {
   state = { language: 'english', color: 'primary' };
 
-  onLanguageChange = language => {
-    this.setState({ language });
-  }
-
   onColorChange = color => {
     this.setState({ color });
-  }
+  };
 
   render() {
     return (
       <div className='ui container'>
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <div>
-          Select a color:
-          <i className='tint icon red' onClick={() => this.onColorChange('red')} />
-          <i className='tint icon blue' onClick={() => this.onColorChange('primary')} />
-        </div>
-        <ColorContext.Provider value={this.state.color}>
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector/>
+          <div>
+            Select a color:
+            <i
+              className='tint icon red'
+              onClick={() => this.onColorChange('red')}
+            />
+            <i
+              className='tint icon blue'
+              onClick={() => this.onColorChange('primary')}
+            />
+          </div>
+          <ColorContext.Provider value={this.state.color}>
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
-
+          </ColorContext.Provider>
+        </LanguageStore>
         {/* 
         Always gets the hardcoded value from here
         <LanguageContext.Provider value='dutch'>
@@ -40,7 +41,7 @@ class App extends Component {
         <UserCreate /> 
         */}
       </div>
-    )
+    );
   }
 }
 
--- translate-Redux-to-Context/src/context/LanguageContext.js
+++ translate-Redux-to-Context/src/context/LanguageContext.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 const Context = React.createContext('english');
 
-class LanguageStore extends Component {
-  
+export class LanguageStore extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 }
+
+export default Context;
