37cdde33a0ee0867120003369a0a911b43cfae71
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-05 16:32:17 +0200

    Pull data from multiple contexts

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class App extends Component {
-  state = { language: 'english' };
+  state = { language: 'english', color: 'primary' };
 
   onLanguageChange = language => {
     this.setState({ language });
+  }
+
+  onColorChange = color => {
+    this.setState({ color });
   }
 
   render() {
@@ -17,10 +22,17 @@ class App extends Component {
           <i className='flag us' onClick={() => this.onLanguageChange('english')} />
           <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
-        
+        <div>
+          Select a color:
+          <i className='tint icon red' onClick={() => this.onColorChange('red')} />
+          <i className='tint icon blue' onClick={() => this.onColorChange('primary')} />
+        </div>
+        <ColorContext.Provider value={this.state.color}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+
         {/* 
         Always gets the hardcoded value from here
         <LanguageContext.Provider value='dutch'>
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class Button extends Component {
   renderSubmit(value) {
     return value === 'english' ? 'Submit' : 'Voorleggen';
   }
 
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
     return (
-      <button className='ui button primary'>
-      <LanguageContext.Consumer>
-        {(value) => this.renderSubmit(value)}
-      </LanguageContext.Consumer>
-      </button>
-    )
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
