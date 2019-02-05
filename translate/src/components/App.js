fd7bd13d8e37452a41b3b56b8b5b4d7655bdbe6e
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-05 14:48:51 +0200

    Create 4 simple components

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-
+import UserCreate from './UserCreate';
 
 class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
   render() {
     return (
-      <div className='ui container'>App</div>
+      <div className='ui container'>
+        <div>
+          Select a language:
+          <i className='flag us' onClick={() => this.onLanguageChange('english')} />
+          <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+        <UserCreate />
+      </div>
     )
   }
 }
---
+++ translate/src/components/Button.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return <button className='ui button primary'>Submit</button>
+  }
+}
+
+export default Button;
