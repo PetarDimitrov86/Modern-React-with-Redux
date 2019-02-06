0eb6e7bccb4ecbc7ce2b4c2bc8662819dadf3195
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-06 11:31:24 +0200

    Fetch data through componentDidMount class function

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
   const [resource, setResource] = useState('posts');
@@ -13,7 +14,7 @@ const App = () => {
           onClick={() => setResource('todos')}>
           TODOs</button>
       </div>
-      {resource}
+      <ResourceList resource={resource}/>
     </div>
   );
 }
---
+++ hooks-simple/src/components/ResourceList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ResourceList extends Component {
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+    this.setState({ resources: response.data });
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+      this.setState({ resources: response.data });
+    }
+  }
+
+  render() {
+    return (
+      <div>{this.state.resources.length}</div>
+    );
+  }
+}
+
+export default ResourceList;
