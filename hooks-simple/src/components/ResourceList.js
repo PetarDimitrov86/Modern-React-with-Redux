1d8639f4f641ce6731409f2de10211860cb65593
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-06 13:51:23 +0200

    Adjust useEffect function. Display list of results

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -2,20 +2,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ResourceList = ({ resource }) => {
-  const [resources, setResources] = useState('resources');
+  const [resources, setResources] = useState([]);
 
   const fetchResource = async (resource) => {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
     setResources(response.data);
   }
 
+  // if we pass empty array, it will work exactly as using componentDidMount 
+  // if we dont use second argument, the function will be called non-stop
+  // if the values in the array remain the same, the function will be called only once
+  // we cannot directly place the body of the fetchResource function, as useEffect function would not allow it
   useEffect(() => {
     fetchResource(resource);
-  }, [])
+  }, [resource]);
 
   return (
-    <div>{resources.length}</div>
+    <ul>
+      {resources.map((record) => {
+        return <li key={record.id}>{record.title}</li>
+      })}
+    </ul>
   );
 }
 
