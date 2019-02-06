15d5cfbdc5944d8cb68743de106e28ba6a95c52d
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-02-06 10:30:25 +0200

    Add state to the app class component. Implement basic button functionality

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { resource: 'posts' };
+
   render() {
     return (
-      <div className='ui container'>App</div>
+      <div className='ui container'>
+        <div>
+          <button className='ui button primary'
+            onClick={() => this.setState({ resource: 'posts' })}>
+            Posts</button>
+          <button className='ui button primary'
+            onClick={() => this.setState({ resource: 'todos' })}>
+            TODOs</button>
+        </div>
+        {this.state.resource}
+      </div>
     );
   }
 }
