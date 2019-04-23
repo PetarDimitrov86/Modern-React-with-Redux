8dc3feaae2cfb4edf128cbcff41dd41142918efc
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-23 19:15:56 +0300

    Include simple enzyme tests. Use absolute paths to components

--- 13-testing/src/components/App.js
+++ 13-testing/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import CommentBox from './CommentBox';
-import CommentList from './CommentList';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 export default () => {
   return (
--- 13-testing/src/components/__tests__/App.test.js
+++ 13-testing/src/components/__tests__/App.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+import '../setupTests';
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
-
-  expect(div.innerHTML).toContain('Comment Box');
-
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
+})
