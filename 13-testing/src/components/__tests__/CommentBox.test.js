17ab44952801b44a10c454b9794473b9e830378c
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-24 14:13:23 +0300

    Write tests for text area. Use describe function for common beforeEach

--- 13-testing/src/components/__tests__/CommentBox.test.js
+++ 13-testing/src/components/__tests__/CommentBox.test.js
@@ -14,7 +14,29 @@ afterEach(() => {
   wrapped.unmount();
 });
 
-it('has a test area and a button', () => {
+it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('clears the text area after form submit', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
