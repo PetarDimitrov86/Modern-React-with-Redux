2b22b0c9557d36b85f87e0198cdf669c5b4651fc
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-25 14:01:09 +0300

    Add simple functionality for signing in and out

--- 13-testing/src/components/App.js
+++ 13-testing/src/components/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
-export default () => {
-  return (
-    <div>
-      <Route path='/post' component={CommentBox}/>
-      <Route path='/' exact component={CommentList}/>
-    </div>
-  );
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path='/post' component={CommentBox} />
+        <Route path='/' exact component={CommentList} />
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
