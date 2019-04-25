2e8345b3425c3d7ce6351f56ef4eb2ae78b0c87e
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-25 10:30:18 +0300

    Implement CommentList component. Add tests for it

--- 13-testing/src/components/CommentList.js
+++ 13-testing/src/components/CommentList.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <div>
-      Comment List
-    </div>
-  );
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
