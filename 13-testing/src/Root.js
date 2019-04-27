11830738fba32dc165922ac282da1f5f3851dab6
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-27 23:54:57 +0300

    Add a second middleware. Use state validation

--- 13-testing/src/Root.js
+++ 13-testing/src/Root.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(async)
+    applyMiddleware(async, stateValidator)
   );
   return <Provider store={store}>{children}</Provider>;
 };
---
+++ 13-testing/src/middlewares/stateValidator.js
@@ -0,0 +1,10 @@
+import tv4 from 'tv4';
+import stateSchema from './stateSchema';
+
+export default ({ dispatch, getState }) => next => action => {
+  next(action);
+ 
+  if(!tv4.validate(getState(), stateSchema)) {
+    console.warn('Invalid state schema detected')
+  };
+}
