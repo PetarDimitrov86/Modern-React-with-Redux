a00bce2dc706d2787e1302ba4b06b6f4f1afe59b
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-28 22:45:50 +0300

    Create boilerplate setup for a basic express server

---
+++ 13-testing/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/* '}));
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
