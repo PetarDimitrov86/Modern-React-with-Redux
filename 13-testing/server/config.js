25826af3a4a2adf01d16166f8acf782cfbba5feb
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-04-29 18:31:09 +0300

    Create JSON Web Token. Return it as result of signup

---
+++ 13-testing/server/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: 'fkdsfkjdsjh874354'
+}
--- 13-testing/server/controllers/authentication.js
+++ 13-testing/server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -23,7 +30,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if (err) { return next(err); }
 
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   })
 }
