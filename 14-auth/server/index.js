be39b72d91cbecfc627b33b656e7d560303c8caa
PetarDimitrov86 <lbmikesmith@gmail.com>
2019-05-02 23:25:21 +0300

    Fix issue with mongodb conneciton

--- 14-auth/server/index.js
+++ 14-auth/server/index.js
@@ -7,18 +7,20 @@ const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//const session = require('express-session');
-//const MongoStore = require('connect-mongo')(session);
+// !!! IMPORTANT: Please also write your IP address in the Security/IP Whitelist section
 
 const MONGO_URI =
   '';
 mongoose.Promise = global.Promise;
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(morgan('combined'));
 app.use(cors());
-app.use(bodyParser.json({ type: '*/* ' }));
+app.use(bodyParser.json());
 router(app);
 
 const port = process.env.PORT || 3090;
