9f5368e073edefd5a908096c605796df562e9ef7
PetarDimitrov86 <lbmikesmith@gmail.com>
2020-12-25 20:01:49 +0200

    add simple styling and render list of items

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'Front-end framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'It is popular'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use it by creating components'
+    },
+]
 
 export default () => {
-    return <h1>Widgets App</h1>
+    return (
+    <div>
+        <Accordion items={items}/>
+    </div>)
 };
---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Accordion = ({ items }) => {
+    const renderedItems = items.map(item => {
+        return <React.Fragment key={item.title}>
+            <div className="title active">
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className="content active">
+                <p>
+                    {item.content}
+                </p>
+            </div>
+        </React.Fragment>
+    })
+
+    return <div className="ui styled accordion">
+        {renderedItems}
+        </div>
+};
+
+export default Accordion;
