725e2fbe568f9efb31498aa4a2e38c6c2665ad87
PetarDimitrov86 <lbmikesmith@gmail.com>
2020-12-25 21:50:52 +0200

    simple useState hook example

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    const renderedItems = items.map((item, index) => {
         return <React.Fragment key={item.title}>
-            <div className="title active">
+            <div className="title active"
+            onClick={() => onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
@@ -17,6 +24,7 @@ const Accordion = ({ items }) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
+        <h1>{activeIndex}</h1>
         </div>
 };
 
