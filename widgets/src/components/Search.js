bd857604c1b057bd6185fe6845653a756479e52b
PetarDimitrov86 <lbmikesmith@gmail.com>
2020-12-25 22:49:28 +0200

    methods for using async methods in useeffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState("");
-    
+
+    useEffect(() => {
+        // Method 1 (best practice):
+        const searchWiki = async () => {
+            await axios.get('url');
+        };
+
+        searchWiki();
+
+        // Method 2
+        (async () => {
+            await axios.get('url');
+        })();
+
+        // Method 3 without async
+        axios.get('url')
+        .then((response) => {
+            console.log(response);
+        })
+    }, [term]);
+
     return (
         <div>
             <div className="ui form">
